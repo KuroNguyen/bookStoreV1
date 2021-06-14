ea12377687c04652c284d851af0b8e1062f2c87b
TRỰC NGUYỄN CHÁNH <chanhtruck13@gmail.com.vn>
2021-06-14 08:23:19 +0700

    Initial commit

---
+++ src/main/java/com/kuro/bookStoreV1/BookStoreV1Application.java
@@ -0,0 +1,13 @@
+package com.kuro.bookStoreV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreV1Application.class, args);
+	}
+
+}
